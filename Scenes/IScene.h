7e05e8901a4a552d21d4fd6babdd7a254f3ef097
kura495 <104417866+kura495@users.noreply.github.com>
2023-11-14 10:15:45 +0900

    make IScene File

--- Scenes/IScene.h
+++ Scenes/IScene.h
@@ -1,1 +1,23 @@
 #pragma once
+enum SECNE {TITLE,STAGE,CLEAR};
+
+class IScene {
+public:
+	virtual ~IScene();
+
+	virtual void Init() = 0;
+	virtual void Update() = 0;
+	virtual void Draw() = 0;
+
+	int GetSceneNo();
+
+protected:
+	static int sceneNo;
+private:
+};
+
+IScene::IScene() {}
+
+IScene::~IScene() {}
+
+inline int IScene::GetSceneNo() { return 0; }
